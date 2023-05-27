bb01ad9e40541fa9bf01e00f02044ef1c4ac9b71
neko <4761762+dotneko@users.noreply.github.com>
2023-05-27 13:48:51 +0000

    Add playMove basic validation

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -13,7 +13,10 @@ import (
 // )
 
 var (
-	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
-	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
-	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
+	ErrInvalidBlack         = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
+	ErrInvalidRed           = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+	ErrGameNotParseable     = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
+	ErrInvalidGameIndex     = sdkerrors.Register(ModuleName, 1103, "game index is invalid")
+	ErrInvalidPositionIndex = sdkerrors.Register(ModuleName, 1104, "position index is invalid")
+	ErrMoveAbsent           = sdkerrors.Register(ModuleName, 1105, "there is no move")
 )
