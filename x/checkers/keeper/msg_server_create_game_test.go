4463c58440d2eefd966738fac061f77493d78ab3
neko <4761762+dotneko@users.noreply.github.com>
2023-05-27 13:25:46 +0000

    Update keeper test with default genesis

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,14 +1,25 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	keepertest "github.com/dotneko/checkers/testutil/keeper"
+	"github.com/dotneko/checkers/x/checkers"
+	"github.com/dotneko/checkers/x/checkers/keeper"
 	"github.com/dotneko/checkers/x/checkers/types"
 	"github.com/stretchr/testify/require"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
+	msgServer, _, context := setupMsgServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
@@ -16,6 +27,6 @@ func TestCreateGame(t *testing.T) {
 	})
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "", //TODO: update with a proper value when updated
+		GameIndex: "1", //TODO: update with a proper value when updated
 	}, *createResponse)
 }
