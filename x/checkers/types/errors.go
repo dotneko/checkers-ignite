ee834da93671fe4e397c3220215c3b6c1add39ca
neko <4761762+dotneko@users.noreply.github.com>
2023-05-28 00:22:14 +0000

    Implement playMove

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -19,4 +19,8 @@ var (
 	ErrInvalidGameIndex     = sdkerrors.Register(ModuleName, 1103, "game index is invalid")
 	ErrInvalidPositionIndex = sdkerrors.Register(ModuleName, 1104, "position index is invalid")
 	ErrMoveAbsent           = sdkerrors.Register(ModuleName, 1105, "there is no move")
+	ErrGameNotFound         = sdkerrors.Register(ModuleName, 1106, "game by id not found")
+	ErrCreatorNotPlayer     = sdkerrors.Register(ModuleName, 1107, "message creator is not a player")
+	ErrNotPlayerTurn        = sdkerrors.Register(ModuleName, 1108, "player tried to play out of turn")
+	ErrWrongMove            = sdkerrors.Register(ModuleName, 1109, "wrong move")
 )
