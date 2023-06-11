de2167bed61075f8a8190313ce00c8eccc813593
dotneko <4761762+dotneko@users.noreply.github.com>
2023-06-11 03:08:59 +0000

    FIFO integration

--- x/checkers/keeper/stored_game_in_fifo.go
+++ x/checkers/keeper/stored_game_in_fifo.go
@@ -25,7 +25,7 @@ func (k Keeper) RemoveFromFifo(ctx sdk.Context, game *types.StoredGame, info *ty
 	if game.AfterIndex != types.NoFifoIndex {
 		afterElement, found := k.GetStoredGame(ctx, game.AfterIndex)
 		if !found {
-			panic("Element after in Fifo was not found")
+			panic("Element after in Fifo wasd not found")
 		}
 		afterElement.BeforeIndex = game.BeforeIndex
 		k.SetStoredGame(ctx, afterElement)
@@ -38,33 +38,6 @@ func (k Keeper) RemoveFromFifo(ctx sdk.Context, game *types.StoredGame, info *ty
 	}
 	game.BeforeIndex = types.NoFifoIndex
 	game.AfterIndex = types.NoFifoIndex
-}
-
-func (k Keeper) SendToFifoTail(ctx sdk.Context, game *types.StoredGame, info *types.SystemInfo) {
-	if info.FifoHeadIndex == types.NoFifoIndex && info.FifoTailIndex == types.NoFifoIndex {
-		game.BeforeIndex = types.NoFifoIndex
-		game.AfterIndex = types.NoFifoIndex
-		info.FifoHeadIndex = game.Index
-		info.FifoTailIndex = game.Index
-	} else if info.FifoHeadIndex == types.NoFifoIndex || info.FifoTailIndex == types.NoFifoIndex {
-		panic("Fifo should have both head and tail or none")
-	} else if info.FifoTailIndex == game.Index {
-		// Nothing to do, already at tail
-	} else {
-		// Snip game out
-		k.RemoveFromFifo(ctx, game, info)
-
-		// Now add to tail
-		currentTail, found := k.GetStoredGame(ctx, info.FifoTailIndex)
-		if !found {
-			panic("Current Fifo tail was not found")
-		}
-		currentTail.AfterIndex = game.Index
-		k.SetStoredGame(ctx, currentTail)
-
-		game.BeforeIndex = currentTail.Index
-		info.FifoTailIndex = game.Index
-	}
 }
 
 func (k Keeper) SendToFifoTail(ctx sdk.Context, game *types.StoredGame, info *types.SystemInfo) {
