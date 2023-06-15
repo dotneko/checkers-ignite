6cc5f83937d39a8fa493e303ed1ee45fe1a2fb00
dotneko <4761762+dotneko@users.noreply.github.com>
2023-06-15 01:36:01 +0000

    Save wager and add arg to cli

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -32,6 +32,7 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 		MoveCount:   0,
 		BeforeIndex: types.NoFifoIndex,
 		AfterIndex:  types.NoFifoIndex,
+		Wager:       msg.Wager,
 	}
 
 	err := storedGame.Validate()
@@ -50,6 +51,7 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 			sdk.NewAttribute(types.GameCreatedEventGameIndex, newIndex),
 			sdk.NewAttribute(types.GameCreatedEventBlack, msg.Black),
 			sdk.NewAttribute(types.GameCreatedEventRed, msg.Red),
+			sdk.NewAttribute(types.GameCreatedEventWager, strconv.FormatUint(msg.Wager, 10)),
 		),
 	)
 
