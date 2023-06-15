4fee6cbc8b88b9c35ca539786e797c263437ac24
dotneko <4761762+dotneko@users.noreply.github.com>
2023-06-15 12:02:45 +0000

    After ignite chain serve

--- vue/src/store/generated/dotneko/checkers/dotneko.checkers.checkers/module/rest.ts
+++ vue/src/store/generated/dotneko/checkers/dotneko.checkers.checkers/module/rest.ts
@@ -66,11 +66,21 @@ export interface CheckersStoredGame {
   red?: string;
   winner?: string;
   deadline?: string;
+
+  /** @format uint64 */
+  moveCount?: string;
+  beforeIndex?: string;
+  afterIndex?: string;
+
+  /** @format uint64 */
+  wager?: string;
 }
 
 export interface CheckersSystemInfo {
   /** @format uint64 */
   nextId?: string;
+  fifoHeadIndex?: string;
+  fifoTailIndex?: string;
 }
 
 export interface ProtobufAny {
