903bdf75ca800933af1e0920c7021f0ad2674df2
dotneko <4761762+dotneko@users.noreply.github.com>
2023-06-17 03:26:29 +0000

    Add helpers for money checking

--- tests/integration/checkers/keeper/keeper_integration_suite_test.go
+++ tests/integration/checkers/keeper/keeper_integration_suite_test.go
@@ -2,12 +2,18 @@ package keeper_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	checkersapp "github.com/dotneko/checkers/app"
+	"github.com/dotneko/checkers/x/checkers/keeper"
 	"github.com/dotneko/checkers/x/checkers/testutil"
 	"github.com/dotneko/checkers/x/checkers/types"
 	"github.com/stretchr/testify/suite"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 const (
@@ -36,4 +42,65 @@ var (
 
 func TestCheckersKeeperTestSuite(t *testing.T) {
 	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (suite *IntegrationTestSuite) SetupTest() {
+	app := checkersapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now()})
+
+	app.AccountKeeper.SetParams(ctx, authtypes.DefaultParams())
+	app.BankKeeper.SetParams(ctx, banktypes.DefaultParams())
+	checkersModuleAddress = app.AccountKeeper.GetModuleAddress(types.ModuleName).String()
+
+	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelper, app.CheckersKeeper)
+	queryClient := types.NewQueryClient(queryHelper)
+
+	suite.app = app
+	suite.msgServer = keeper.NewMsgServerImpl(app.CheckersKeeper)
+	suite.ctx = ctx
+	suite.queryClient = queryClient
+}
+
+func makeBalance(address string, balance int64) banktypes.Balance {
+	return banktypes.Balance{
+		Address: address,
+		Coins: sdk.Coins{
+			sdk.Coin{
+				Denom:  sdk.DefaultBondDenom,
+				Amount: sdk.NewInt(balance),
+			},
+		},
+	}
+}
+
+func getBankGenesis() *banktypes.GenesisState {
+	coins := []banktypes.Balance{
+		makeBalance(alice, balAlice),
+		makeBalance(bob, balBob),
+		makeBalance(carol, balCarol),
+	}
+	supply := banktypes.Supply{
+		Total: coins[0].Coins.Add(coins[1].Coins...).Add(coins[2].Coins...),
+	}
+
+	state := banktypes.NewGenesisState(
+		banktypes.DefaultParams(),
+		coins,
+		supply.Total,
+		[]banktypes.Metadata{})
+
+	return state
+}
+
+func (suite *IntegrationTestSuite) setupSuiteWithBalances() {
+	suite.app.BankKeeper.InitGenesis(suite.ctx, getBankGenesis())
+}
+
+func (suite *IntegrationTestSuite) RequireBankBalance(expected int, atAddress string) {
+	sdkAdd, err := sdk.AccAddressFromBech32(atAddress)
+	suite.Require().Nil(err, "Failed to parse address: %s", atAddress)
+	suite.Require().Equal(
+		int64(expected),
+		suite.app.BankKeeper.GetBalance(suite.ctx, sdkAdd, sdk.DefaultBondDenom).Amount.Int64())
 }
